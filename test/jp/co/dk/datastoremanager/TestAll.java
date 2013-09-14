c350943ad5c75584dbdf3a78e372224761c83d4b
dk <dk@425babaa-d5dd-46a5-afc5-8c713db7da9f>
2013-09-14 20:26:28 +0000

    DataStoreManager作成中-テストコード実装中
    git-svn-id: file:///opt/svn/repository/projects/trunk@252 425babaa-d5dd-46a5-afc5-8c713db7da9f

--- test/jp/co/dk/datastoremanager/TestAll.java
+++ test/jp/co/dk/datastoremanager/TestAll.java
@@ -3,6 +3,12 @@ package jp.co.dk.datastoremanager;
 import jp.co.dk.datastoremanager.database.TestDataBaseAccessParameter;
 import jp.co.dk.datastoremanager.database.TestDataBaseDataStore;
 import jp.co.dk.datastoremanager.database.TestDataBaseDriverConstants;
+import jp.co.dk.datastoremanager.database.TestDataBaseRecord;
+import jp.co.dk.datastoremanager.database.TestDateSqlParameter;
+import jp.co.dk.datastoremanager.database.TestIntSqlParameter;
+import jp.co.dk.datastoremanager.database.TestSql;
+import jp.co.dk.datastoremanager.database.TestStringSqlParameter;
+import jp.co.dk.datastoremanager.database.TestTransaction;
 import jp.co.dk.datastoremanager.exception.TestDataStoreManagerException;
 import jp.co.dk.datastoremanager.property.TestDataStoreManagerProperty;
 
@@ -19,6 +25,12 @@ import org.junit.runners.Suite.SuiteClasses;
 	TestDataBaseAccessParameter.class,
 	TestDataBaseDataStore.class,
 	TestDataBaseDriverConstants.class,
+	TestDataBaseRecord.class,
+	TestDateSqlParameter.class,
+	TestIntSqlParameter.class,
+	TestSql.class,
+	TestStringSqlParameter.class,
+	TestTransaction.class,
 	
 	TestDataStoreManagerException.class,
 	
