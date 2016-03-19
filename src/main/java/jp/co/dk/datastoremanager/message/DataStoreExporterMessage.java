ccc5f5fedbaec2f6b7609fbc7e429967d077b4a8
Daisuke Kanno <daisuke6106@gmail.com>
2016-03-19 12:02:48 +0900

    メッセージを補正

--- src/main/java/jp/co/dk/datastoremanager/message/DataStoreExporterMessage.java
+++ src/main/java/jp/co/dk/datastoremanager/message/DataStoreExporterMessage.java
@@ -14,19 +14,19 @@ public class DataStoreExporterMessage extends AbstractMessage{
 	public static final DataStoreExporterMessage FAILED_TO_READ_FILE = new DataStoreExporterMessage("E001");
 	
 	/** 変数値が設定されていません。 */
-	public static final DataStoreExporterMessage VALIABLE_IS_NOT_SET = new DataStoreExporterMessage("E001");
+	public static final DataStoreExporterMessage VALIABLE_IS_NOT_SET = new DataStoreExporterMessage("E002");
 	
 	/** 変数型に不明な値が設定されています。 */
-	public static final DataStoreExporterMessage VALIABLE_TYPE_IS_UNKNOWN = new DataStoreExporterMessage("E002");
+	public static final DataStoreExporterMessage VALIABLE_TYPE_IS_UNKNOWN = new DataStoreExporterMessage("E003");
 	
 	/** 変数に不正な値が設定されています。 */
-	public static final DataStoreExporterMessage VALIABLE_IS_CHECK_ERROR = new DataStoreExporterMessage("E003");
+	public static final DataStoreExporterMessage VALIABLE_IS_CHECK_ERROR = new DataStoreExporterMessage("E004");
 	
 	/** 変数の値が指定されていません。変数名=[{0}] */
-	public static final DataStoreExporterMessage VALUE_IS_NOT_SET = new DataStoreExporterMessage("E004");
+	public static final DataStoreExporterMessage VALUE_IS_NOT_SET = new DataStoreExporterMessage("E005");
 	
 	/** パラメータのフォーマットが不正です。パラメータ=[{0}] */
-	public static final DataStoreExporterMessage PARAMETER_FORMAT_IS_INVALID = new DataStoreExporterMessage("E005");
+	public static final DataStoreExporterMessage PARAMETER_FORMAT_IS_INVALID = new DataStoreExporterMessage("E006");
 	
 	// === HTML関連 ===
 	/** HTMLの生成に失敗しました。 */
